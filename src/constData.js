d9e5d8a2adbd355249c6b97b0505876c49cac53c
Akhil-5292 <aps.ca9@gmail.com>
2023-05-02 14:19:45 +0530

    home page updated

--- src/constData.js
+++ src/constData.js
@@ -12,7 +12,7 @@ export const data = [{
     }],
     category: 'nonveg',
     image: 'https://www.dominos.co.in//files/items/Pepper_Barbeque_&_Onion.jpg',
-    description: 'Pepper Barbeque Chicken I Cheese'
+    description: 'This BBQ chicken pizza has spicy barbecue sauce, diced chicken, peppers, onion, and cilantro, all covered with cheese and baked to bubbly goodness!'
 }, {
     name: 'NON VEG SUPREME',
     varients: [
@@ -27,7 +27,7 @@ export const data = [{
     }],
     category: 'nonveg',
     image: 'https://www.dominos.co.in/theme2/front/images/menu-images/my-nonveg.webp',
-    description: 'Pepper Barbeque Chicken I Cheese'
+    description: 'The supreme pizza, or as we call it here in the Northeast, pizza with the works, is the ultimate pizza and features six classic toppings. The supreme pizza is traditionally topped with pepperoni, sausage, meatballs, mushrooms, green bell peppers, and onions.'
 }, {
     name: 'GOLDEN CORN PIZZA',
     varients: [
@@ -42,7 +42,7 @@ export const data = [{
     }],
     category: 'nonveg',
     image: 'https://curefoods-images.eatfit.in/tr:w-600,ar-0.8,c_fit//image/singles/eat/meals/EAT6672/primary/3_1.jpg',
-    description: 'Pepper Barbeque Chicken I Cheese'
+    description: 'Imagine a pizza perfectly baked with fresh dough and topped with crunchy sweet corn and tangy sauce, along with melting cheese '
 }, {
     name: 'JALAPENO & RED PARIKA PIZZA',
     varients: [
@@ -57,9 +57,9 @@ export const data = [{
     }],
     category: 'nonveg',
     image: 'https://howtothisandthat.com/wp-content/uploads/2018/03/keto-diet-tips-2.png',
-    description: 'Pepper Barbeque Chicken I Cheese'
+    description: 'This incredible jalapeno pizza is the best way to enjoy this mighty pepper on a cheesy pie! Its full of savory, spicy flavor.'
 },{
-    name: 'MARGERITA',
+    name: 'MARGHERITA',
     varients: [
         'small',
         'medium',
@@ -72,7 +72,7 @@ export const data = [{
     }],
     category: 'nonveg',
     image: 'https://www.dominos.co.in//files/items/Paneer_Makhni.jpg',
-    description: 'Pepper Barbeque Chicken I Cheese'
+    description: 'pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients, baked quickly—usually, in a commercial setting, using a wood-fired oven heated to a very high temperature—and served hot ...'
 },{
     name: 'DOUBLE CHEESE MARGHERITA PIZZA',
     varients: [
@@ -87,5 +87,5 @@ export const data = [{
     }],
     category: 'nonveg',
     image: 'https://product-assets.faasos.io/production/product/image_1631125907990_Low_IFC_Double_Cheese_Pizza.jpg',
-    description: 'Pepper Barbeque Chicken I Cheese'
+    description: 'Margherita pizza is known for its ingredients representing the colours of the Italian flag. These ingredients include red tomato sauce, white mozzarella and fresh green basil. When all of these delicious flavours are combined on a hand-kneaded pizza base, a universally-adored pizza is created.'
 }]
